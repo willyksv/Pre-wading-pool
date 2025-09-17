2189940edbd234f480163dd7a6c66932b608707f
Willy <keosouvanhwilly2@gmail.com>
2025-09-17 17:30:33 +0200

    update

--- day 7/hangman.py
+++ day 7/hangman.py
@@ -14,4 +14,17 @@ import english_words
 from english_words import(get_english_words_set)
 web2lowerset = get_english_words_set(['web2'], lower=True)
 word = random.choice(list(web2lowerset))
+number_of_character = len(str(word))
 print(word)
+print(number_of_character)
+
+def number_to_guess(word):
+    hidden = ""
+    for character in range(number_of_character - 1): #juste par soucis de presentation pour pouvoir ajouter le "_."
+        hidden = "_ " + hidden
+    hidden = hidden + "_."
+    return hidden #vu que c'est une variable locale elle existe uniquement dans ma fonction number_to_guess donc j'utilise un return pour la faire sortir
+
+hidden = number_to_guess(word)
+print(hidden)
+# print(number_to_guess(word))
