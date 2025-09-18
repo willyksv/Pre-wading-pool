88ec1d4081eab41c729738ab8df017e7d2ff7a5a
Willy <keosouvanhwilly2@gmail.com>
2025-09-18 13:09:35 +0200

    update hangman

--- day 7/hangman.py
+++ day 7/hangman.py
@@ -2,7 +2,12 @@
 https://pypi.org/project/english-words/1.1.0/
 https://pypi.org/project/english-words/
 https://stackoverflow.com/questions/4859292/how-can-i-get-a-random-key-value-pair-from-a-dictionary
+https://www.geeksforgeeks.org/python/python-replace-to-k-at-ith-index-in-string/
 """
+
+
+
+
 
 hp = 11 
 if hp >= 12:
@@ -16,15 +21,41 @@ web2lowerset = get_english_words_set(['web2'], lower=True)
 word = random.choice(list(web2lowerset))
 number_of_character = len(str(word))
 print(word)
-print(number_of_character)
+# print(number_of_character)
 
 def number_to_guess(word):
     hidden = ""
     for character in range(number_of_character - 1): #juste par soucis de presentation pour pouvoir ajouter le "_."
         hidden = "_ " + hidden
-    hidden = hidden + "_."
+    hidden = hidden + "_." #au moment de sortir de la boucle
     return hidden #vu que c'est une variable locale elle existe uniquement dans ma fonction number_to_guess donc j'utilise un return pour la faire sortir
-
 hidden = number_to_guess(word)
 print(hidden)
-# print(number_to_guess(word))
+
+print(len(hidden))
+
+letter= "a"
+def trouve_index():
+    index = -1
+    idx = []
+    for i in word:
+        if i == letter:
+            index = word.find("a",index+1)
+            idx.append(int(index *2))
+    return idx
+idx = trouve_index()
+print(idx)
+
+def replace_blank(hidden):
+    for i in range(len(hidden)):
+        i = (i)
+        n=0
+        for i in idx:
+            if i == idx[n]:
+                i = int(i)
+                n += 1
+                hidden = hidden[:i]+ letter + hidden[i+1:]            
+        else:
+            hidden
+        return hidden
+print(replace_blank(hidden))
